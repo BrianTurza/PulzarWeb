f5e024c399b63f8643d80217eb6b1cf4c54ae622
Brian Turza <43517172+BrianTurza@users.noreply.github.com>
2022-06-17 00:21:16 +0200

    Update connect.php

--- web/connect.php
+++ web/connect.php
@@ -1,10 +1,10 @@
 <?PHP
 
 // connect to the database
-  $db_host = "mariadb103.websupport.sk";
-  $db_user = "pulzar";
-  $db_pass = "Pz\$data123";
-  $db_name = "pulzar";
+  $db_host = $_ENV['host'];
+  $db_user = $_ENV['username'];
+  $db_pass = $_ENV['password'];
+  $db_name = $_ENV['database'];
   $db =  mysqli_connect($db_host,$db_user,$db_pass,$db_name, 3313);
 
   if (!$db) {
